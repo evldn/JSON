3e12f5578a4a2e9bbef9790aa916fca6b5b87853
Evgeniy Volodin <zheka.az@mail.ru>
2023-04-28 12:06:21 +0300

    - Added enum TypeValue - Added class constructors - Virtual methods are implemented

--- JSONLib/IValue.cpp
+++ JSONLib/IValue.cpp
@@ -1,1 +1,10 @@
 #include "IValue.h"
+
+namespace JSONLib {
+	IValue* IterListValue::Next() {
+		if (!hasNext()) throw - 1;
+		IValue* val = temp->val;
+		temp = temp->next;
+		return val;
+	}
+}
--- JSONLib/IValue.h
+++ JSONLib/IValue.h
@@ -6,9 +6,14 @@ namespace JSONLib
 {
 	class IterIValue;
 
+	enum TypeValue {
+		Val,
+		ListVal
+	};
+
 	class IValue {
 	public:
-		virtual int getType() = 0;
+		virtual TypeValue getType() = 0;
 		virtual std::string getKey() = 0;
 		virtual std::string getValue() = 0;
 		virtual IterIValue* iterator() = 0;
@@ -19,7 +24,7 @@ namespace JSONLib
 		Link* prev;
 		IValue* val;
 	public:
-		Link(IValue* _val) : next(nullptr), prev(nullptr), val(_val) {}
+		Link(IValue* _val) : next(nullptr), prev(nullptr), val(_val) { }
 	};
 
 	class IterIValue {
@@ -31,42 +36,41 @@ namespace JSONLib
 	class IterValue : public IterIValue {
 		IValue* val;
 	public:
-		IterValue(IValue* _val);
-		bool hasNext() override;
-		IValue* Next() override;
+		IterValue(IValue* _val) : val(_val) { }
+		bool hasNext() { return false; }
+		IValue* Next() { return val; }
 	};
 
 	class IterListValue : public IterIValue {
 		Link* temp;
-		Link* tail;
 	public:
-		IterListValue(Link* head);
-		bool hasNext() override;
-		IValue* Next() override;
+		IterListValue(Link* head) : temp(head) { }
+		bool hasNext() { return temp != nullptr; }
+		IValue* Next();
 	};
 
 	class Value :public IValue {
 		std::string key;
 		std::string val;
 	public:
-		Value(std::string _key, std::string _val);
-		int getType();
-		std::string getKey();
-		std::string getValue();
-		IterIValue* iterator();
+		Value(std::string _key, std::string _val) : key(_key), val(_val) { }
+		TypeValue getType() { return Val; }
+		std::string getKey() { return key; }
+		std::string getValue() { return val; }
+		IterIValue* iterator() { return new IterValue(this); }
 	};
 
 	class ListValue :public IValue {
 		std::string key;
 		std::string value;
-	public:
 		Link* head;
 		Link* tail;
-		ListValue(std::string _key, std::string _value);
+	public:
+		ListValue(std::string _key, std::string _value) : key(_key), value(_value), head(nullptr), tail(nullptr) { }
 		~ListValue();
-		int getType();
-		std::string getKey();
+		TypeValue getType() { return ListVal; }
+		std::string getKey() { return key; }
+		std::string getValue() { return value; }
 		IterIValue* iterator() { return new IterListValue(head); }
-		std::string getValue();
 	};
 }
