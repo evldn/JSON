fb8843b8d71deed4c37462116b33dc520466e1d9
Evgeniy Volodin <zheka.az@mail.ru>
2023-04-28 11:45:37 +0300

    - An abstract IValue class created - Link structure created - Inheritor classes of Value and ListValue created - An abstract IterIValue class created - Inheritor classes IterIValue and IterListValue created - Added fields and methods to these classes

--- JSONLib/IValue.h
+++ JSONLib/IValue.h
@@ -1,1 +1,72 @@
 #pragma once
+
+#include <string>
+
+namespace JSONLib
+{
+	class IterIValue;
+
+	class IValue {
+	public:
+		virtual int getType() = 0;
+		virtual std::string getKey() = 0;
+		virtual std::string getValue() = 0;
+		virtual IterIValue* iterator() = 0;
+	};
+
+	struct Link {
+		Link* next;
+		Link* prev;
+		IValue* val;
+	public:
+		Link(IValue* _val) : next(nullptr), prev(nullptr), val(_val) {}
+	};
+
+	class IterIValue {
+	public:
+		virtual bool hasNext() = 0;
+		virtual IValue* Next() = 0;
+	};
+
+	class IterValue : public IterIValue {
+		IValue* val;
+	public:
+		IterValue(IValue* _val);
+		bool hasNext() override;
+		IValue* Next() override;
+	};
+
+	class IterListValue : public IterIValue {
+		Link* temp;
+		Link* tail;
+	public:
+		IterListValue(Link* head);
+		bool hasNext() override;
+		IValue* Next() override;
+	};
+
+	class Value :public IValue {
+		std::string key;
+		std::string val;
+	public:
+		Value(std::string _key, std::string _val);
+		int getType();
+		std::string getKey();
+		std::string getValue();
+		IterIValue* iterator();
+	};
+
+	class ListValue :public IValue {
+		std::string key;
+		std::string value;
+	public:
+		Link* head;
+		Link* tail;
+		ListValue(std::string _key, std::string _value);
+		~ListValue();
+		int getType();
+		std::string getKey();
+		IterIValue* iterator() { return new IterListValue(head); }
+		std::string getValue();
+	};
+}
